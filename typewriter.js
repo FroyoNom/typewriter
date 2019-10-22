9d3af5f5857368563541c90159d308936627c43a
Simon <simonolsen388@gmail.com>
2019-10-22 20:46:52 +0000

    created a function to 'animate' a string writing itself out

---
+++ typewriter.js
@@ -0,0 +1,14 @@
+const sentence = "hello there from lighthouse labs";
+let result = 0;
+const characterFunc = () => {
+  for (const char of sentence) {
+    setTimeout(() => {
+      process.stdout.write(char);
+    }, 1000 + 50 * result);
+    result += 1;
+  }
+  setTimeout(() => {
+    process.stdout.write("\n");
+  }, 1000 + 50 * sentence.length + 1);
+};
+characterFunc();
